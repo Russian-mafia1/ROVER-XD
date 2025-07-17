e49f113429a152f610412c484bdc241fc1873068
Russian-mafia1 <ianokipchirchir@gmail.com>
2025-07-17 21:45:09 +0300

    Update emix-fun.js

--- plugins/emix-fun.js
+++ plugins/emix-fun.js
@@ -31,7 +31,7 @@ cmd({
         let buffer = await getBuffer(imageUrl);
         let sticker = new Sticker(buffer, {
             pack: "Emoji Mix",
-            author: "KAISEN-MD",
+            author: "ROVER-XD",
             type: StickerTypes.FULL,
             categories: ["🤩", "🎉"],
             quality: 75,
